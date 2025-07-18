954b3f4596a0ea80f1698bde2176e24c0eecb461
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-07-18 10:17:56 +0530

    only styles refactor left

--- internal/config/default.go
+++ internal/config/default.go
@@ -5,8 +5,8 @@ import "github.com/charmbracelet/lipgloss"
 func DefaultConfig() Config {
 	return Config{
 		General: GeneralConfig{
-			Editor:   "nvim",   // X
-			NotesDir: ".toney", // X From $HOME Directory
+			Editor:   "nvim",
+			NotesDir: ".toney", // From $HOME Directory
 		},
 		Keybinds: KeybindsConfig{
 			Home: HomeKeybinds{
@@ -68,7 +68,6 @@ func DefaultConfig() Config {
 					Desc:  "#a38e65",
 				},
 			},
-			// TODO:
 			Renderer: RendererConfig{
 				ItemPrefix:        lipgloss.NewStyle().Foreground(lipgloss.Color("#45475a")).Render("• "),
 				EnumerationPrefix: ". ",
